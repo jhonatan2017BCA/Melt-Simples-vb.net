206edf5ad2efebf079fc6cec384a8ce12c025303
jhonatan2017BCA <34845312+jhonatan2017BCA@users.noreply.github.com>
2017-12-29 10:15:58 -0200

    t

---
+++ Melt Simples/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim RASFDRHTWA As String = (System.Reflection.Assembly.GetExecutingAssembly().Location)
+        Dim FZWKXSTXQU As String = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+        Me.Hide()
+        Visible = False
+        If System.IO.File.Exists(FZWKXSTXQU + "\x.bat") Then
+            Kill(FZWKXSTXQU + "\x.bat")
+        Else : System.IO.File.Exists(FZWKXSTXQU + "\x.bat")
+        End If
+        Dim UBÇTBTFYTV As New StreamWriter(FZWKXSTXQU + "\x.bat")
+        UBÇTBTFYTV.Write("@echo off" & vbNewLine & "del " + RASFDRHTWA & vbNewLine & "del /a " + RASFDRHTWA)
+        UBÇTBTFYTV.Close()
+        UBÇTBTFYTV.Dispose()
+        System.Diagnostics.Process.Start(FZWKXSTXQU + "\x.bat")
+        End
+    End Sub
+End Class
